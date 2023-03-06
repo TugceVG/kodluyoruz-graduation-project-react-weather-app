be34fbc5cd9c6365be8b76adfed21a6c8bb9b8cd
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-03-06 22:35:20 +0100

    Updates

--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const BASE_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = 'b039e459b151af4b3a20ad890b34942e';
-
-export const STORAGE_KEYS = {
-    IS_LOGGED_IN: 'isLoggedIn',
-    SEARCHED_LOCATIONS: 'searchedLocation',
-};
-
-export const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
